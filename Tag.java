b58c26af1c3ae7b7cd31d438b34f77b0c65a5e0e
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2020-11-03 14:52:48 +0530

    Added Tag.java

---
+++ Tag.java
@@ -0,0 +1,10 @@
+public class Tag {
+    public static String[][] StringList = new String[100][100];
+    public static int[] no_tags = new int[100];
+    
+    public Tag()
+    {
+      
+    }
+    
+}
